e80a6d305337237694bc9f8eed8dd1e17d9ebe15
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-09-22 17:05:57 +1200

    Delete backend/src/services directory

--- backend/src/services/database.service.ts
+++
@@ -1,7 +0,0 @@
-// import { Injectable, OnModuleInit } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import * as mongodb from 'mongodb';
-// import * as dotenv from 'dotenv';
-
-// @Injectable() 
-// export class DatabaseService implements OnModuleInit {}
