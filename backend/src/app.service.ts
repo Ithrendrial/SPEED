87f32ff91c11b56ca28ae57831aa47b41c15a3b3
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-09-22 17:04:16 +1200

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,18 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Article, ArticleSchema } from './article/schemas/article.schema';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @InjectModel(Article.name) private readonly articleModel: Model<Article>,
-  ) {}
-
-  isDbConnected(): boolean {
-    // Check the Mongoose connection status
-    return this.articleModel.db.readyState === 1;
-  }
   getHello(): string {
     return 'Hello World!';
   }
