f25d8b37f98f6b0bae9db28e0b0949ce952d4a16
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-09-22 17:03:32 +1200

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,18 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/check-db-connection')
-  checkDbConnection() {
-    // You can add more complex checks here if needed
-    if (this.appService.isDbConnected()) {
-      return 'Connected to MongoDB';
-    } else {
-      return 'Not connected to MongoDB';
-    }
   }
 }
