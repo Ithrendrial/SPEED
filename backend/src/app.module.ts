8c2d457206a2eea9ea8f2612178dd1c6f8bcb3a0
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-09-22 17:03:57 +1200

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { Article, ArticleSchema } from './article/schemas/article.schema';
-import { ArticleModule } from './article/article.module';
-import * as dotenv from 'dotenv';
 
 @Module({
-  imports: [
-    ArticleModule,
-    MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: 'mongodb+srv://matcris81:Lewandowski9@cluster0.d1cz2ne.mongodb.net/?retryWrites=true&w=majority',
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }),
-    }),
-  ],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-// [MongooseModule.forRoot('mongodb+srv://matcris81:Lewandowski9@cluster0.d1cz2ne.mongodb.net/?retryWrites=true&w=majority',)],
