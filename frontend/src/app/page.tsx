23595df948004e1a99246e87f24d715e798440e2
Ithrendrial <rcox2990@gmail.com>
2023-09-22 17:16:33 +1200

    feat: removed dependency

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,4 @@
 import style from "../styles/HomePage.module.css"
-import { BsArrowRightShort } from "react-icons/bs";
 import Link from "next/link";
 
 export default function Home() {
@@ -7,8 +6,8 @@ export default function Home() {
     <div className={ style.page }>
       <div className={ style.heading }>SPEED</div>
       <div className={ style.subheading }>SOFTWARE PRACTICE EMPIRICAL EVIDENCE DATABASE</div>
-      <Link className={ style.link } href={ "/submit" }>SUBMIT AN ARTICLE <BsArrowRightShort className={style.arrow}/></Link>
-      <Link className={ style.link } href={ "/search" }>SEARCH DATABASE <BsArrowRightShort className={style.arrow}/></Link>
+      <Link className={ style.link } href={ "/submit" }>SUBMIT AN ARTICLE </Link>
+      <Link className={ style.link } href={ "/search" }>SEARCH DATABASE </Link>
     </div>
   )
 }
