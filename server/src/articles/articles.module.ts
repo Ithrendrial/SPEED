6b59542f02b7446ac39fb6489a0a6d5438642877
Ithrendrial <rcox2990@gmail.com>
2023-09-24 00:16:47 +1200

    feat: add database files

---
+++ server/src/articles/articles.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Article, ArticleSchema } from "./schemas/articles.schema";
+import { ArticlesController } from "./articles.controller";
+import { ArticlesRepository } from "./articles.repository";
+import { ArticlesService } from "./articles.service";
+
+@Module({
+    imports: [MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }])],
+    controllers: [ArticlesController],
+    providers: [ArticlesService, ArticlesRepository]
+})
+export class ArticleModule {}
---
+++ server/src/articles/articles.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { v4 as uuidv4 } from "uuid";
+import { UpdateArticleDto } from "./dto/update-article.dto";
+import { Article } from "./schemas/articles.schema";
+import { ArticlesRepository } from "./articles.repository";
+
+@Injectable()
+export class ArticlesService {
+    constructor(private readonly articlesRepository: ArticlesRepository) {}
+
+    // Get a specified user by id
+    async getArticleById(articleId: string): Promise<Article> {
+        return this.articlesRepository.findOne({ articleId })
+    }
+
+    // Get all the users
+    async getArticles(): Promise<Article[]> {
+        return this.articlesRepository.find({});
+    }
+
+    // Create a new article when given the title and authors
+    async createArticle(title: string, authors: string[]): Promise<Article> {
+        return this.articlesRepository.create({
+            articleId: uuidv4(),
+            title,
+            authors: authors
+        })
+    }
+
+    // Finds a user by id and applies updates
+    async updateArticle(articleId: string, articleUpdates: UpdateArticleDto): Promise<Article> {
+        return this.articlesRepository.findOneAndUpdate({ articleId }, articleUpdates);
+    }
+}
