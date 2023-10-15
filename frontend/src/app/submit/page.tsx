291d10bd8f45cd5ffcac7e58dcc1de13e66658ff
xmp4998 <83616948+xmp4998@users.noreply.github.com>
2023-10-15 17:07:07 +1300

    New Article Form can post data to db
    Interface change to match the fetch.
    Authors can now insert one or more than one with button to add author.

--- frontend/src/app/submit/page.tsx
+++ frontend/src/app/submit/page.tsx
@@ -1,158 +1,221 @@
 'use client'
 import style from "../../styles/SubmitPage.module.css"
-
 import React, { useState } from 'react';
+import axios from 'axios';
 
 interface FormData {
     title: string;
-    author: string;
-    journal: string;
-    year: number;
-    volume: string;
-    issue: string;
-    pages: number;
-    DOI: string;
+    authors: string[];
+    journal_name: string;
+    publication_date: string;
+    volume: number;
+    issue: number;
+    pages: string;
+    doi: string;
+    moderator_status: string;
+    method: string[];
+    claim: string[];
+    research_type:string[],
+    participant_type: string[];
+    summary: string[];
+    support: string[];
+    published_status: boolean;
+    rating: number[];
 }
 
 const initialFormData: FormData = {
     title: '',
-    author: '',
-    journal: '',
-    year: 0,
-    volume: '',
-    issue: '',
-    pages: 0,
-    DOI: '',
+    authors: [""],
+    journal_name: '',
+    publication_date: '',
+    volume: 0,
+    issue: 0,
+    pages: '',
+    doi: '',
+    moderator_status: "unmoderated",//Need
+    method: [""],
+    claim: [""],
+    research_type: [""],
+    participant_type: [""],
+    summary: [""],
+    support: ["neutral"],//Need
+    published_status: false,//Need
+    rating: [0]
 };
+
+/*const newArticleData =
+    {
+        title: "Testing mob program",
+        authors: ["author 7", "author 8", "author 9"],
+        journal_name: "Journal Name",
+        publication_date: "2012",
+        volume: null,
+        issue: 2,
+        pages: "1-15",
+        doi: "https://doi.org/10.1088/1367",
+        moderator_status: "unmoderated",//Need
+        method: [""],
+        claim: [""],
+        research_type: [""],
+        participant_type: [""],
+        summary: [""],
+        support: ["neutral"],//Need
+        published_status: false,//Need
+        rating: [0]
+    }*/
+// Add other properties as needed
+
+
 
 const NumberForm: React.FC = () => {
     const [formData, setFormData] = useState<FormData>(initialFormData);
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        // Do something with formData, like printing it to the console or sending it to a server.
-        console.log(formData);
+        axios
+            .post('http://localhost:3001/articles', formData)
+            .then((response) => {
+                console.log('Article created successfully:', response.data);
+            })
+            .catch((error) => {
+                console.error('Error creating article:', error);
+            });
     };
 
     const handleChange = (
         e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
     ) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
+        setFormData({
+
+            ...formData,
+            [name]: name === 'authors' ? [value] : value,
+        });
+
+    };
+    const addAuthor = () => {
         setFormData({
             ...formData,
-            [name]: name === 'year' || name === 'pages' ? parseFloat(value) : value,
+            authors: [...formData.authors, ''],
         });
     };
-
     return (
-<div>
-    <div className={style.topic}>
-        <h1> SUBMIT A NEW ARTICLE</h1>
-        </div>
+        <div>
+            <div className={style.topic}>
+                <h1>SUBMIT A NEW ARTICLE</h1>
+            </div>
             <div className={style.formoutline}>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Article Title*<br />
+                        <input
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleChange}
+                            placeholder="Title"
+                            required
+                        />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        Author(s)*<br />
+                        {formData.authors.map((author, index) => (
+                            <input
+                                type="text"
+                                name="authors"
+                                key={index}
+                                value={author}
+                                onChange={(e) => {
+                                    const newAuthors = [...formData.authors];
+                                    newAuthors[index] = e.target.value;
+                                    setFormData({
+                                        ...formData,
+                                        authors: newAuthors,
+                                    });
+                                }}
+                                placeholder="Full Name"
+                                required
+                            />
+                        ))}
+                        <div className={style.button}>
+                        <button type="button" onClick={addAuthor}>Add Author</button>
+                        </div>
+                    </label>
+                    <br />
 
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Article Title*<br/>
-                    <input
-                        type="text"
-                        name="title"
-                        value={formData.title}
-                        onChange={handleChange}
-                        placeholder="Title"
-                        required
-                    />
-                </label>
-                <br/>
-                <label>
-                    Author(S)*<br/>
-                    <input
-                        type="text"
-                        name="author"
-                        value={formData.author}
-                        onChange={handleChange}
-                        placeholder= "Full Name"
-                        required
-                    />
-                </label>
-                <br/>
-
-                <label>
-                    Publication Information
-                    <br/>
-                    <input
-                        type="text"
-                        name="journal"
-                        value={formData.journal}
-                        onChange={handleChange}
-                        placeholder= "Journal Name"
-                    />
-                </label>
-                <br/>
-                <label>
-                    <input
-                        type="number"
-                        name="year"
-                        value={formData.year}
-                        onChange={handleChange}
-                        placeholder="Publication Date"
-                    />
-                </label>
-                <br/>
-                <label>
-
-                    <input
-                        type="text"
-                        name="volume"
-                        value={formData.volume}
-                        onChange={handleChange}
-                        placeholder="Volume"
-                    />
-                </label>
-                <br/>
-                <label>
-
-                    <input
-                        type="text"
-                        name="issue"
-                        value={formData.issue}
-                        onChange={handleChange}
-                        placeholder="Issue"
-                    />
-                </label>
-                <br/>
-                <label>
-
-                    <input
-                        type="number"
-                        name="pages"
-                        value={formData.pages}
-                        onChange={handleChange}
-                        placeholder="Page(S)"
-                    />
-                </label>
-                <br/>
-                <label>
-                    DOI*<br/>
-                    <input
-                        type="text"
-                        name="DOI"
-                        value={formData.DOI}
-                        onChange={handleChange}
-                        placeholder="DOI"
-                        required
-                    />
-                </label>
-                <br/>
-                <br/>
-                <div className={style.button}>
-                <button type="submit">Submit</button>
-                </div>
-            </form>
-    </div>
-     </div>
-
+                    <label>
+                        Publication Information<br />
+                        <input
+                            type="text"
+                            name="journal_name"
+                            value={formData.journal_name}
+                            onChange={handleChange}
+                            placeholder="Journal Name"
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            type="text"
+                            name="publication_date"
+                            value={formData.publication_date}
+                            onChange={handleChange}
+                            placeholder="Publication Date"
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            type="number"
+                            name="volume"
+                            value={formData.volume}
+                            onChange={handleChange}
+                            placeholder="Volume"
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            type="text"
+                            name="issue"
+                            value={formData.issue}
+                            onChange={handleChange}
+                            placeholder="Issue"
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        <input
+                            type="text"
+                            name="pages"
+                            value={formData.pages}
+                            onChange={handleChange}
+                            placeholder="Page(S)"
+                        />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        DOI*<br />
+                        <input
+                            type="text"
+                            name="doi"
+                            value={formData.doi}
+                            onChange={handleChange}
+                            placeholder="DOI"
+                            required
+                        />
+                    </label>
+                    <div className={style.button}>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 };
 
 export default NumberForm;
+
